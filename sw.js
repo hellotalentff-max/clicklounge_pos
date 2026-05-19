f6f33eb7784c7a9261482970c7729789f2a16bc9
hellotalentff-max <hellotalent.ff@gmail.com>
2026-05-19 00:26:19 -0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,34 +1,26 @@
-const CACHE_NAME = 'clicklounge-pos-v41';
+const CACHE_NAME = 'clicklounge-pos-v42';
 
 self.addEventListener('install', e => {
   self.skipWaiting();
-  e.waitUntil(
-    caches.open(CACHE_NAME).then(c => c.addAll(['/'])).catch(() => {})
-  );
+  e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(['/'])).catch(() => {}));
 });
 
 self.addEventListener('activate', e => {
   e.waitUntil(
     caches.keys()
-      .then(keys => Promise.all(
-        keys.filter(k => k !== CACHE_NAME).map(k => {
-          console.log('SW: deleting old cache', k);
-          return caches.delete(k);
-        })
-      ))
+      .then(keys => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))))
       .then(() => self.clients.claim())
   );
 });
 
-// Network-first: always fetch fresh, fall back to cache
 self.addEventListener('fetch', e => {
-  if (e.request.method !== 'GET') return;
+  if(e.request.method !== 'GET') return;
   e.respondWith(
-    fetch(e.request, { cache: 'no-store' })
+    fetch(e.request, {cache:'no-store'})
       .then(res => {
-        if (res && res.status === 200) {
+        if(res && res.status === 200){
           const clone = res.clone();
-          caches.open(CACHE_NAME).then(c => c.put(e.request, clone)).catch(() => {});
+          caches.open(CACHE_NAME).then(c => c.put(e.request, clone)).catch(()=>{});
         }
         return res;
       })
@@ -37,9 +29,7 @@ self.addEventListener('fetch', e => {
 });
 
 self.addEventListener('message', e => {
-  if (e.data && e.data.type === 'FLUSH_QUEUE') {
-    self.clients.matchAll().then(clients =>
-      clients.forEach(c => c.postMessage({ type: 'FLUSH_QUEUE' }))
-    );
+  if(e.data && e.data.type === 'FLUSH_QUEUE'){
+    self.clients.matchAll().then(clients => clients.forEach(c => c.postMessage({type:'FLUSH_QUEUE'})));
   }
 });
